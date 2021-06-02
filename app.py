d1bd39665e220cddfc47f0cbe6389b1444d7aa7e
Andrew Baker <abaker178@gmail.com>
2021-06-02 16:31:11 -0400

    refactor - migrate to postgres

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, jsonify, request
 from datetime import datetime as dt
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 @app.route("/")
 def home():
-    app.config["MONGO_URI"] = f"mongodb+srv://website:v2ZdmFKbXTW5uRw@cranbrook.afp5d.mongodb.net/specials?retryWrites=true&w=majority"
-    mongo = PyMongo(app)
-    
+    return jsonify(db)
+
+@app.route("/specials")
+def specials():    
     month = dt.now().strftime("%B")
     coll = dt.now().strftime("%b%Y").lower()
-    beer = mongo.db[coll].find({"category": "beer"})
-    wine = mongo.db[coll].find({"category": "wine"})
-    spirit = mongo.db[coll].find({"category": "spirit"})
+    beer = db[coll].find({"category": "beer"})
+    wine = db[coll].find({"category": "wine"})
+    spirit = db[coll].find({"category": "spirit"})
     return render_template("specials.html", month=month, beer=beer, wine=wine, spirit=spirit)
 
 @app.route("/staff")
 def staff():
-    app.config["MONGO_URI"] = f"mongodb+srv://website:v2ZdmFKbXTW5uRw@cranbrook.afp5d.mongodb.net/staffDB?retryWrites=true&w=majority"
-    mongo = PyMongo(app)
 
     today = dt.today()
-    staff = mongo.db["staff"].find()
+    staff = db["staff"].find()
     return render_template("staff.html", staff=staff, today=today)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
---
+++ models.py
@@ -0,0 +1,54 @@
+def create_beer(db):
+    class Beer(db.Model):
+        __tablename__ = 'beer'
+
+        id = db.Column(db.Integer, primary_key=True)
+        category = db.Column(db.String(64))
+        brand = db.Column(db.String(64))
+        product = db.Column(db.String(64))
+        volAmt = db.Column(db.Float)
+        volUnit = db.Column(db.String(32))
+        xpack = db.Column(db.Int)
+        medium = db.Column(db.String(64))
+        price = db.Column(db.Float)
+        image = db.Column(db.String(32))
+
+        def __repr__(self):
+            return '<Beer %r>' % (self.name)
+    return Beer
+
+def create_wine(db):
+    class Wine(db.Model):
+        __tablename__ = 'wine'
+
+        id = db.Column(db.Integer, primary_key=True)
+        category = db.Column(db.String(64))
+        brand = db.Column(db.String(64))
+        product = db.Column(db.String(64))
+        volAmt = db.Column(db.Float)
+        volUnit = db.Column(db.String(32))
+        varietals = db.Column(db.String(64))
+        medium = db.Column(db.String(64))
+        price = db.Column(db.Float)
+        image = db.Column(db.String(32))
+
+        def __repr__(self):
+            return '<Wine %r>' % (self.name)
+    return Wine
+
+def create_spirit(db):
+    class Spirit(db.Model):
+        __tablename__ = 'spirits'
+
+        id = db.Column(db.Integer, primary_key=True)
+        category = db.Column(db.String(64))
+        brand = db.Column(db.String(64))
+        product = db.Column(db.String(64))
+        volAmt = db.Column(db.Float)
+        volUnit = db.Column(db.String(32))
+        price = db.Column(db.Float)
+        image = db.Column(db.String(32))
+
+        def __repr__(self):
+            return '<Spirit %r>' % (self.name)
+    return Spirit
