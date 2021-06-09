60ff8b897c9e04a39280e2e8b172fe61f81772ca
Cranbrook Liquors <askus@cranbrookliquors.com>
2021-06-09 15:15:35 -0400

    upload image is functional

--- static/js/new-special.js
+++ static/js/new-special.js
@@ -23,14 +23,36 @@
 
 // Update available fields based on the category that is selected
 function updateFields(category) {
+    // Turn off all fields
     let fields = document.querySelectorAll(".field")
     Array.prototype.slice.call(fields)
         .forEach(field => field.style.display = "none");
     
+    // Only show fields that are an aspect of the chosen category
     let fieldsOfSelected = document.querySelectorAll("."+category);
     Array.prototype.slice.call(fieldsOfSelected)
         .forEach(field => field.style.display = "block");
+
+    // Update the background image for the WYSIWYG
+    let itemImageEle = document.getElementById("item-image");
+    let itemImageURLList = itemImageEle.src.split("/");
+    let itemImageName = itemImageURLList[itemImageURLList.length-1];
+    itemImageName === "stock.jpg" ? itemImageEle.src = `/static/img/specials/${category}/stock.jpg` : false;
 };
+
+/*** Show uploaded image code from https://jsfiddle.net/bootstrapious/8w7a50n2/ ***/
+function readURL(input) {
+    console.log(input);
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            document.getElementById("item-image").src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
 
 /*** FOR BEER ***/
 // Make sure either Bottles or Cans is selected at all times
