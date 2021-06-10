54af91b8f089a295333747f0cffeab91b20cd722
Andrew Baker <abaker178@gmail.com>
2021-06-10 19:10:05 -0400

    refactored form validation to be handled by Vue

--- static/js/new-special.js
+++ static/js/new-special.js
@@ -1,29 +1,8 @@
-/*** Form validation code from https://getbootstrap.com/docs/5.0/forms/validation/ ***/
-/*** Changes made by Andrew Baker to fit the requirements ***/
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch Special Form we want to apply custom Bootstrap validation styles to
-    let forms = document.querySelectorAll('.needs-validation')
-
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-            form.classList.add('was-validated')
-        }, false)
-    })
-})()
-/*** End Form validation code from https://getbootstrap.com/docs/5.0/forms/validation/ ***/
-
 /*** Vue.js Code to dynamically populate the Preview Section ***/
 var newSpecial = new Vue({
     el: '#new-special',
     data: {
+        errors: [],
         category: "beer",
         brand: "",
         product: "",
@@ -57,8 +36,21 @@ var newSpecial = new Vue({
             } else {
                 itemImageURL.src = `/static/img/specials/${newSpecial.category}/stock.jpg`;
             }
-        }
+        },
         /*** End Show uploaded image code from https://jsfiddle.net/bootstrapious/8w7a50n2/ ***/
+
+        /*** Form validation code from https://getbootstrap.com/docs/5.0/forms/validation/ ***/
+        /*** Changes made by Andrew Baker to fit the requirements ***/
+        checkForm: event => {
+            // Fetch Special Form we want to apply custom Bootstrap validation styles to
+            let form = document.getElementById('special-form')
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+        }
+        /*** End Form validation code from https://getbootstrap.com/docs/5.0/forms/validation/ ***/
     },
     delimiters: ["[[ "," ]]"]
 });
