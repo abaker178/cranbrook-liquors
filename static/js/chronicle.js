19c4f1d3b07e85fa6fab8add9dad39a11538eccb
Andrew Baker <abaker178@gmail.com>
2021-07-01 08:36:50 -0400

    saving before work

--- static/js/chronicle.js
+++ static/js/chronicle.js
@@ -3,31 +3,52 @@ var monthInput = document.getElementById("month-in").attributes.value.value
 var monthEle = document.getElementById("month")
 monthEle.value = monthInput
 
+var apiURL = "api?category=";
+
 // Adds the selected month to the URL as a parameter
 // The app will handle the routing and DB query
 function updateSpecials(month) {
     window.location = `../chronicle?month=${month}`;
 };
 
-// D3 for drag-n-drop editor
-var apiURL = "api?category=";
+// Get the correct order of the specials for the Chronicle Ad
+function getChronicleOrder(beer, wine, spirit) {
+    var spec_order = [];
+    spec_order.push(
+        wine[0],
+        spirit.slice(0,3),
+        wine.slice(1,4),
+        spirit.slice(3,6),
+        wine.slice(4,7),
+        spirit.slice(6,9),
+        beer.slice(0,4),
+        wine.slice(7,),
+        spirit.slice(9,),
+        beer.slice(4,)
+    );
+    return spec_order
+}
 
-var svg = d3.select("svg");
-svg.append("use")
-    .attr("href", "#pointer")
-    .attr("x", 50)
-    .attr("y", 50)
-    .attr("fill", "#039BE5")
-    .attr("stroke", "#039BE5")
-    .attr("stroke-width", "1px");
 
-var dragHandler = d3.drag()
-    .on("drag", function () {
-        d3.select(this)
-            .attr("x", d3.event.x)
-            .attr("y", d3.event.y);
-    });
- 
-dragHandler(svg.selectAll("use"));
-
+// Drag & Drop Functionality
+function allowDrop(ev) {
+    ev.preventDefault();
+}
   
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+  
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+async function getSpecials(month) {
+    const beer = await fetch(`/api?category=beer&month=${month}`);
+    const wine = await fetch(`/api?category=wine&month=${month}`);
+    const spirit = await fetch(`/api?category=spirit&month=${month}`);
+    const myJson = await beer.json();
+    // do something with myJson
+  }
